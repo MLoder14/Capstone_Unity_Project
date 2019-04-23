ec234d9fc9d5c314e9c117bea105eb87e93af5f3
MLoder14 <38083857+MLoder14@users.noreply.github.com>
2019-04-23 12:43:00 -0700

    Fixed Pouch
    Fixed the pouch script.

--- Capstone_Project(3D)/Assets/Models/Raycsting.cs
+++ Capstone_Project(3D)/Assets/Models/Raycsting.cs
@@ -6,9 +6,10 @@ public class Raycsting : MonoBehaviour
 {
     // access the collider object from the raycassting
     //use that to change that specific pouch bool.
-    public GameObject pouch;
+    //public GameObject pouch;
     //public Animator pouchAnimator;
 
+    private Animator pouch;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,21 +22,27 @@ public class Raycsting : MonoBehaviour
         RaycastHit hit;
         float theDistance;
 
+
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
         Debug.DrawRay(transform.position, forward, Color.green);
 
+        //if the ray cast hits something shoot it out in the debug log for testing.
         if (Physics.Raycast(transform.position, (forward), out hit));
         {
             theDistance = hit.distance;
             print(theDistance + " " + hit.collider.gameObject.name);
 
+            //checks the ray cast to see if it hits the pouch.
             if (hit.collider.gameObject.tag == "Pouch")
             {
+                //save the pouch animator for shutting it off later.
+                pouch = hit.collider.gameObject.GetComponent<Animator>();
                 hit.collider.gameObject.GetComponent<Animator>().SetBool("Open", true);
             }
+            //if the raycast doesn't hit the pouch, close it.
             else if (hit.collider.gameObject.tag != "Pouch")
             {
-                hit.collider.gameObject.GetComponent<Animator>().SetBool("Open", false);
+                pouch.SetBool("Open", false);
             }
         }
 
