21954289a440fe5f3db18617f21a04bd7b479c3c
MLoder14 <38083857+MLoder14@users.noreply.github.com>
2019-04-25 13:06:54 -0700

    Added Rees's work to the project.

---
+++ Capstone_Project(3D)/Assets/LockPrefab/LockScripts/LockController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    //public enum pins {pin1,pin2,pin3,pin4};
+    private int selectedPin;
+    private int currentActivePin;
+    public GameObject[] pinObjects;
+    [HideInInspector]
+    public bool[] pinsSet;
+    [HideInInspector]
+    public float necessaryMinTension;
+    public float minTensionPerPin;
+    public GameObject pickMovePoint;
+    public GameObject wrenchMovePoint;
+    public float wrenchMoveRate;
+
+    private Vector3 pickPointStart;
+    private Vector3 pickPointStartRotation;
+    private Vector3 wrenchRotationStartEuler;
+    public bool unlocked;
+    private bool initializedActive;
+    private MovePin[] pinControllers;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wrenchRotationStartEuler = new Vector3(wrenchMovePoint.transform.eulerAngles.x, wrenchMovePoint.transform.eulerAngles.y, wrenchMovePoint.transform.eulerAngles.z);
+        pickPointStart = new Vector3(pickMovePoint.transform.position.x, pickMovePoint.transform.position.y, pickMovePoint.transform.position.z);
+        pickPointStartRotation = new Vector3(pickMovePoint.transform.eulerAngles.x, pickMovePoint.transform.eulerAngles.y, pickMovePoint.transform.eulerAngles.z);
+        pinsSet = new bool[4];
+        initializedActive = false;
+        selectedPin = 0;
+        currentActivePin = 0;
+        pinObjects[selectedPin].GetComponent<MovePin>().pinActive = true;
+        unlocked = false;
+        pinControllers = new MovePin[4];
+        for(int i = 0; i < 4; i++)
+        {
+            pinControllers[i] = pinObjects[i].GetComponent<MovePin>();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pickMovePoint.transform.parent != pinControllers[selectedPin].pinBottom.transform)
+        {
+            movePickUnderPin();
+        }
+        if(pinControllers[selectedPin].pinActive == false && initializedActive == false)
+        {
+            pinControllers[selectedPin].pinActive = true;
+            pinControllers[selectedPin].setHeightColorBlue();
+            initializedActive = true;
+        }
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            selectedPin = Mathf.Clamp(selectedPin - 1, 0, 3);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (pinControllers[selectedPin].pinSet == true)
+            {
+                selectedPin = Mathf.Clamp(selectedPin + 1, 0, 3);
+            }
+        }
+        //Debug.Log("SelectedPin: " + selectedPin + " CurrentActivePin: " + currentActivePin);
+        if (unlocked == false)
+        {
+            if (pinsSet[0] == true && pinsSet[1] == true && pinsSet[2] == true && pinsSet[3] == true)
+            {
+                unlocked = true;
+            }
+        }
+        if(selectedPin != currentActivePin)
+        {
+            pinControllers[currentActivePin].setHeightColorRed();
+            pinControllers[currentActivePin].pinActive = false;
+            pinControllers[selectedPin].pinActive = true;
+            currentActivePin = selectedPin;
+            pinControllers[currentActivePin].tensionWrenchForceCurrent = necessaryMinTension;
+            pinControllers[currentActivePin].setHeightColorBlue();
+            movePickUnderPin();
+            //pickMovePoint.transform.position = new Vector3(pickPointStart.x, pickPointStart.y, pickPointStart.z);
+        }
+        if (pinControllers[currentActivePin].tensionWrenchForceCurrent < necessaryMinTension)
+        {
+            necessaryMinTension = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i != currentActivePin)
+                {
+                    Debug.Log("Current active pin: " + currentActivePin.ToString());
+                    pinsSet[i] = false;
+                    pinControllers[i].StartCoroutine("failureMoveToStartPosition");
+                    
+                }
+            }
+        }
+        rotateWrench();
+        movePick();
+
+    }
+
+    public void calculateMinTension()
+    {
+        necessaryMinTension += minTensionPerPin;
+        Debug.Log("necmintension: " + necessaryMinTension);
+    }
+
+    public void movePickUnderPin()
+    {
+        pickMovePoint.transform.position = new Vector3(pinObjects[currentActivePin].GetComponent<MovePin>().pinHeight.transform.position.x, pickPointStart.y, pickPointStart.z);
+        pickMovePoint.transform.eulerAngles = new Vector3(pickPointStartRotation.x, pickPointStartRotation.y, 0.75f * selectedPin);
+        pickMovePoint.gameObject.transform.SetParent(pinObjects[currentActivePin].GetComponent<MovePin>().pinBottom.transform);
+        
+    }
+
+    public void movePick()
+    {
+        /*if(pinObjects[currentActivePin].GetComponent<MovePin>().pinCurMoveDist > 0)
+        {
+            pickMovePoint.transform.position = new Vector3(pickMovePoint.transform.position.x, pickPointStart.y + pinObjects[currentActivePin].GetComponent<MovePin>().pinCurMoveDist, pickMovePoint.transform.position.z);
+        }
+        else
+        {
+            pickMovePoint.transform.position = new Vector3(pickPointStart.x, pickPointStart.y, pickPointStart.z);
+        }*/
+        
+
+    }
+
+    public void rotateWrench()
+    {
+        wrenchMovePoint.transform.eulerAngles = new Vector3(wrenchRotationStartEuler.x + (pinObjects[currentActivePin].GetComponent<MovePin>().tensionWrenchForceCurrent/wrenchMoveRate), wrenchRotationStartEuler.y, wrenchRotationStartEuler.z);
+    }
+}
---
+++ Capstone_Project(3D)/Assets/LockPrefab/LockScripts/MovePin.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePin : MonoBehaviour
+{
+    public GameObject spring;
+    public GameObject pinTop;
+    public GameObject pinBottom;
+    public GameObject maxPinHeight;
+    public GameObject minPinHeight;
+    public GameObject pinOpenHeight;
+    public GameObject pinHeight;
+    public GameObject lockParent;
+    
+    
+    public float tensionWrenchForceBuild;
+    public float maxTension;
+    public float minTension;
+    public float frictionBuild;
+    public float minFriction;
+    public float springForceMultiplyer;
+    public float springScaleBuild;
+    public bool pinSet;
+    public float movementResolution = 1;
+    public int pinNumber;
+    public float pinSuccessRotation;
+    [HideInInspector]
+    public float tensionWrenchForceCurrent = 1;
+    [HideInInspector]
+    public bool pinActive;
+    [HideInInspector]
+    private float springForce;
+
+    private LockController lockController;
+    private float pickForce;
+    private float currentFriction = 1;
+    private float maxFriction;
+    private Vector3 startPosition;
+    private Vector3 pinTopStartPos;
+    private Vector3 pinBottomStartPos;
+    private springValues springVals;
+    private float pinMaxMoveDist;
+    [HideInInspector]
+    public float pinCurMoveDist;
+    private Renderer pinHeightRenderer;
+    private CheckUnlockZoneCollision unlockZoneCollision;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lockController = lockParent.GetComponent<LockController>();
+        pinHeightRenderer = pinHeight.GetComponent<Renderer>();
+        unlockZoneCollision = pinHeight.GetComponent<CheckUnlockZoneCollision>();
+        pinSet = false;
+        pinActive = false;
+        pinMaxMoveDist = maxPinHeight.transform.position.y - minPinHeight.transform.position.y;
+        pickForce = pinMaxMoveDist / movementResolution;
+        springForce = pickForce * springForceMultiplyer;
+        springVals = spring.GetComponent<springValues>();
+        maxFriction = maxTension * frictionBuild;
+        currentFriction = 1;
+        startPosition = new Vector3(pinHeight.transform.position.x, pinHeight.transform.position.y, pinHeight.transform.position.z);
+        pinTopStartPos = new Vector3(pinTop.transform.position.x, pinTop.transform.position.y, pinTop.transform.position.z);
+        pinBottomStartPos = new Vector3(pinBottom.transform.position.x, pinBottom.transform.position.y, pinBottom.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForce = 0.0f;
+        if (pinActive == true && pinSet == false)
+        { 
+            
+            if (Input.GetKey(KeyCode.S))
+            {
+                tensionWrenchForceCurrent = Mathf.Clamp(tensionWrenchForceCurrent - tensionWrenchForceBuild, minTension, maxTension);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                tensionWrenchForceCurrent = Mathf.Clamp(tensionWrenchForceCurrent + tensionWrenchForceBuild, minTension, maxTension);
+            }
+            currentFriction = Mathf.Clamp(tensionWrenchForceCurrent * frictionBuild, minFriction, maxFriction);
+            //Debug.Log("TensionWrenchForceCurrent: " + tensionWrenchForceCurrent + " currentFriction: " + currentFriction);
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (pinHeight.transform.position.y < maxPinHeight.transform.position.y)
+                {
+                    if (pinHeight.transform.position.y + pickForce / currentFriction <= maxPinHeight.transform.position.y)
+                    {
+                        moveForce += pickForce / currentFriction;
+                    }
+                    else
+                    {
+                        moveForce += maxPinHeight.transform.position.y - pinHeight.transform.position.y;
+                    }
+                }
+
+            }
+            else if (pinHeight.transform.position.y > startPosition.y)
+            {
+                if (pinHeight.transform.position.y - springForce / currentFriction > startPosition.y)
+                {
+                    //pinHeight.transform.position = new Vector3(0, -springForce, 0) + pinHeight.transform.position;
+                    //moveEntirePin(springForce);
+                    moveForce -= springForce / currentFriction;
+                }
+                else
+                {
+                    //moveForce += startPosition.y - pinHeight.transform.position.y;
+                    moveToStartPosition();
+                }
+            }
+            if (currentFriction == maxFriction)
+            {
+                moveForce = 0.0f;
+            }
+            moveEntirePin(moveForce);
+            
+        }
+        
+        //if(Mathf.Abs(pinHeight.transform.position.y - pinOpenHeight.transform.position.y) <= successTolerance)
+        if (unlockZoneCollision.inUnlockZone == true && pinSet == false)
+        {
+            pinHeightRenderer.material.color = Color.green;
+            if(currentFriction == maxFriction)
+            {
+                pinSet = true;
+                pinTop.transform.eulerAngles = new Vector3(pinTop.transform.eulerAngles.x - pinSuccessRotation, pinTop.transform.eulerAngles.y, pinTop.transform.eulerAngles.z);
+                lockController.pinsSet[pinNumber] = true;
+                lockController.calculateMinTension();
+            }
+        }
+        else if(pinActive && pinHeightRenderer.material.color != Color.blue)
+        {
+            setHeightColorBlue();
+        }
+        /*else if(pinHeight.GetComponent<Renderer>().material.color != Color.red)
+        {
+            pinHeight.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if(pinActive == true)
+        {
+            pinOpenHeight.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            pinOpenHeight.GetComponent<Renderer>().material.color = Color.white;
+        }*/
+
+    }
+
+    void moveEntirePin(float inForce)
+    {
+        spring.transform.position = spring.transform.position + new Vector3(0, inForce / 2, 0);
+        if (inForce != 0)
+        {
+            pinCurMoveDist = pinHeight.transform.position.y - minPinHeight.transform.position.y;
+            float movePercentage = pinCurMoveDist / pinMaxMoveDist;
+            float newScale = (springVals.scaleYMax - springVals.scaleYMin) * movePercentage;
+            //spring.transform.localScale = new Vector3(springVals.startScale.x, Mathf.Clamp(spring.transform.localScale.y - springScaleBuild * Mathf.Sign(inForce)*(inForce/2), springVals.scaleYMin, springVals.scaleYMax), springVals.startScale.z);
+            if (inForce > 0)
+            {
+                spring.transform.localScale = new Vector3(springVals.startScale.x, springVals.startScale.y - (newScale * Mathf.Sign(inForce)), springVals.startScale.z);
+            }
+            else
+            {
+                spring.transform.localScale = new Vector3(springVals.startScale.x, springVals.startScale.y - newScale, springVals.startScale.z);
+            }
+        }
+        else if (pinHeight.transform.position == startPosition)
+        {
+            spring.transform.localScale = new Vector3(springVals.startScale.x, springVals.startScale.y, springVals.startScale.z);
+        }
+        pinTop.transform.position = pinTop.transform.position + new Vector3(0, inForce, 0);
+        pinBottom.transform.position = pinBottom.transform.position + new Vector3(0, inForce, 0);
+    }
+
+    private float calculateSubdivSize()
+    {
+        return (maxPinHeight.transform.position.y - minPinHeight.transform.position.y) / movementResolution;
+    }
+
+    public IEnumerator failureMoveToStartPosition()
+    {
+        if (pinSet == true)
+        {
+            pinTop.transform.eulerAngles = new Vector3(pinTop.transform.eulerAngles.x + pinSuccessRotation, pinTop.transform.eulerAngles.y, pinTop.transform.eulerAngles.z);
+        }
+        currentFriction = minFriction;
+        tensionWrenchForceCurrent = minTension;
+        setHeightColorRed();
+        if (pinHeight.transform.position.y > startPosition.y)
+        {
+            float moveForce = 0.0f;
+            int counter = 0;
+            while (pinHeight.transform.position.y > startPosition.y && counter < 100)
+            {
+                moveForce = 0.0f;
+                if (pinHeight.transform.position.y - springForce / currentFriction > startPosition.y)
+                {
+                    moveForce -= springForce / currentFriction;
+                    Debug.Log("move Force: " + moveForce.ToString());
+                    moveEntirePin(moveForce);
+                }
+                else
+                {
+                    pinTop.transform.position = new Vector3(pinTopStartPos.x, pinTopStartPos.y, pinTopStartPos.z);
+                    pinBottom.transform.position = new Vector3(pinBottomStartPos.x, pinBottomStartPos.y, pinBottomStartPos.z);
+                    spring.transform.localScale = new Vector3(springVals.startScale.x, springVals.startScale.y, springVals.startScale.z);
+                    spring.transform.position = new Vector3(springVals.startPosition.x, springVals.startPosition.y, springVals.startPosition.z);
+                    Debug.Log("move Force: " + moveForce.ToString());
+                }
+                yield return null;
+                counter++;
+            }
+            Debug.Log("setting pin = false");
+            pinSet = false;
+        }
+        //Debug.Log("current friction: " + currentFriction.ToString() + "currentTension: " + tensionWrenchForceCurrent);
+    }
+
+    public void moveToStartPosition()
+    {
+        if (pinHeight.transform.position.y > startPosition.y)
+        {
+            pinTop.transform.position = new Vector3(pinTopStartPos.x, pinTopStartPos.y, pinTopStartPos.z);
+            pinBottom.transform.position = new Vector3(pinBottomStartPos.x, pinBottomStartPos.y, pinBottomStartPos.z);
+            spring.transform.localScale = new Vector3(springVals.startScale.x, springVals.startScale.y, springVals.startScale.z);
+            spring.transform.position = new Vector3(springVals.startPosition.x, springVals.startPosition.y, springVals.startPosition.z);
+
+            //yield return null;
+        }
+        //Debug.Log("current friction: " + currentFriction.ToString() + "currentTension: " + tensionWrenchForceCurrent);
+    }
+
+    public void setHeightColorBlue()
+    {
+        pinHeightRenderer.material.color = Color.blue;
+    }
+
+    public void setHeightColorRed()
+    {
+        pinHeightRenderer.material.color = Color.red;
+    }
+}
