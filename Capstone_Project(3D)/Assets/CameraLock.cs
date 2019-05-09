90ae746126dad4febd1d0cb7bc9a42230f7803da
Marcus <loderm14@my.lanecc.edu>
2019-05-09 01:02:13 -0700

    Camera tweaks

--- Capstone_Project(3D)/Assets/CameraLock.cs
+++ Capstone_Project(3D)/Assets/CameraLock.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraLock : MonoBehaviour
 {
+    public Vector3 position = new Vector3 (0,0,0);
+
+
     // Start is called before the first frame update
     void Start()
     {
