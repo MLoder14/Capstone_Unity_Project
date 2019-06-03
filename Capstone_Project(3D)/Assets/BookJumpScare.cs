e8d3fab945c42d873f63e695cfa45dfe8dddbd2d
Marcus <loderm14@my.lanecc.edu>
2019-06-03 00:55:19 -0700

    Made Jump Scares

---
+++ Capstone_Project(3D)/Assets/BookJumpScare.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookJumpScare : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            rb.AddForce(0f, 0f, 10f, ForceMode.Impulse);
+        }
+    }
+}
