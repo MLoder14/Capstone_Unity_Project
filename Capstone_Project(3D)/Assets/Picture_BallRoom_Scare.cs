b727d0d2f1917742359dc9b0cc94f5752b12f3ad
MLoder14 <38083857+MLoder14@users.noreply.github.com>
2019-06-05 12:41:40 -0700

    Jump Scares Added

---
+++ Capstone_Project(3D)/Assets/Picture_BallRoom_Scare.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picture_BallRoom_Scare : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            rb.isKinematic = false;
+            rb.AddForce(10f, 0f, 0f, ForceMode.Impulse);
+        }
+    }
+
+}
